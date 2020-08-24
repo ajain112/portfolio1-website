2bb63cb7df8677c6652ecffcc6fd9234186953cc
ajain112 <60506100+ajain112@users.noreply.github.com>
2020-08-24 20:50:09 +0530

    Add files via upload

---
+++ portfolio/js/1.js
@@ -0,0 +1,22 @@
+function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
+
+  function on1() {
+    document.getElementById("overla").style.display = "block";
+  }
+  
+  function off1() {
+    document.getElementById("overla").style.display = "none";
+  }
+  function on2() {
+    document.getElementById("overl").style.display = "block";
+  }
+  
+  function off2() {
+    document.getElementById("overl").style.display = "none";
+  }
